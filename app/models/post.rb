428ef9237324e2d66c56ee7b8d90eaf7183967dd
DEJK <dejkemily@gmail.com>
2015-02-01 09:51:33 +0000

    blog demo in nitrous

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence:true, length: {maximum: 10}
+  belongs_to :category
   belongs_to :user
   has_many :comments
+  mount_uploader :avatar, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129132419) do
+ActiveRecord::Schema.define(version: 20150201084419) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "msg"
@@ -26,6 +32,8 @@ ActiveRecord::Schema.define(version: 20150129132419) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
+    t.string   "avatar"
   end
 
   create_table "users", force: true do |t|
